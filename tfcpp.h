9fa86af96439d015ea302bdde894f1e523161109
于林峰 <86186021+yulinfeng16@users.noreply.github.com>
2023-02-12 11:29:39 +0800

    Add files via upload

---
+++ tfcpp.h
@@ -0,0 +1,53 @@
+#ifndef TFCPP_H
+#define TFCPP_H
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<ctime>
+#include<stdio.h>
+#include<vector>
+#include<queue>
+#include<windows.h>
+#define elif else if
+using namespace std;
+void timeload(int keytime,string a,int z)
+{
+	cout<<" ";
+	if(z==0)
+	{
+	for(int i=0;i<keytime;i++)
+	{
+		cout<<"\b|";
+		Sleep(250);
+		cout<<"\b/";
+		Sleep(250);
+		cout<<"\b¡ª";
+		Sleep(250);
+		cout<<"\b\\";
+		Sleep(250);
+	}}if(z==1){for(int i=0;i<keytime;i++)
+	{
+		cout<<"\b¡¤";
+		Sleep(250);
+		cout<<"\b\b¨E";
+		Sleep(250);
+		cout<<"\b¡­";
+		Sleep(250);
+		cout<<"\b¡Ë";
+		Sleep(250);
+	}}
+	if(z==2)
+	{
+		for(int i=0;i<keytime*2;i++)
+	{
+		cout<<"\b+";
+		Sleep(250);
+		cout<<"\b¡Á";
+		Sleep(250);
+		cout<<"\b";
+	}
+	}
+	cout<<"\b"<<a;
+}
+#endif
